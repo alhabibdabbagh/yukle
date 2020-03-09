3067ad2b08d389cf82781df25dedf4641caa29c0
alhabibdabbagh <58683636+alhabibdabbagh@users.noreply.github.com>
2020-03-09 22:31:13 +0300

    merhaba

---
+++ yukle/src/yukle/HAbuhdsa.java
@@ -0,0 +1,10 @@
+package yukle;
+
+public class HAbuhdsa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("dkbjaskjdbkasjbkjasdbkjsabkdkjbas");
+	}
+
+}
